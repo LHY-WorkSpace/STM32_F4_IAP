9f22a947c810bafded261dec8f6d7ce8a2f69e34
lihaiyan <1607762095@qq.com>
2021-06-03 22:06:39 +0800

    123

--- drive/IAP.c
+++ drive/IAP.c
@@ -22,23 +22,23 @@ void Goto_UserCode(u32 UserCodeAddr)
 
 
 
-
+__set_MSP(*(vu32*)USERCODE_BASE_ADDR);
 
 }
 
 
 
 
-void IAP_Init()
+void IAP_Start()
 {
 
-    NVIC_SetVectorTable(NVIC_VectTab_FLASH,);
+    
 
 
 
 
 
-
+    NVIC_SetVectorTable(NVIC_VectTab_FLASH,BOOTLOADER_CODE_SIZE); //将向量表重新定位到应用程序的起始地址
 
     
 }
@@ -49,7 +49,7 @@ void main()
  	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);	
 	USART1_Init(115200,USART_DATA_8bit,USART_STOP_1bit,USART_PARTYT_NO);
 	Delay_Init();  //延时函数必须靠前，因为有些函数操作需要延时
-
+    File_FATFSInit();
     
 
 
--- drive/IAP.h
+++ drive/IAP.h
@@ -1,17 +1,20 @@
 #ifndef IAP_H
 #define IAP_H
 
-
+//IAP程序起始地址
 #define BOOTLOADER_BASE_ADDR    ((u32)0x08000000)      
-#define BOOTLOADER_CODE_SIZE    ((u32)0x00003C00)   //15K
-
-#define USERCODE_BASE_ADDR      ((u32)0x08003D00) 
-#define USERCODE_BASE_ADD       ((u32)0x08000000) 
+#define BOOTLOADER_CODE_SIZE    ((u32)0x00003C00)   //大小 15K
 
 
 
+//应用程序起始地址
+#define USERCODE_BASE_ADDR      ( (u32)BOOTLOADER_BASE_ADDR + BOOTLOADER_CODE_SIZE ) 
 
 
+
+
+
+void 
 
 
 
